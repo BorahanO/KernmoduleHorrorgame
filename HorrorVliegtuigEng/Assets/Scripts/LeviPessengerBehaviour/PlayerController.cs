dcd01fcf1e571b2b134487b52ce1a72d82ed611b
Michael van der Sanden <michael.mvdsanden@gmail.com>
2023-05-25 16:37:52 +0200

    nog meer quests

--- HorrorVliegtuigEng/Assets/Scripts/LeviPessengerBehaviour/PlayerController.cs
+++ HorrorVliegtuigEng/Assets/Scripts/LeviPessengerBehaviour/PlayerController.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    [SerializeField] private LayerMask mask;
+    [SerializeField] private LayerMask convoMask;
+    [SerializeField] private LayerMask trashMask;
     [SerializeField] private float range;
+    private GameObject trashObject;
     private GameObject convoObject;
     private StartConvo convo;
+    public Camera cam;
 
     [Space]
     public QuestManager manager;
@@ -25,9 +28,10 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            var ray = new Ray(player.transform.position, player.transform.forward);
+            //var ray = new Ray(player.transform.position, player.transform.forward);
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, range, mask))
+            if (Physics.Raycast(ray, out hit, range, convoMask))
             {
                 convoObject = hit.transform.gameObject;
                 convo = convoObject.GetComponentInParent<StartConvo>();
@@ -36,6 +40,11 @@ public class PlayerController : MonoBehaviour
                     StartCoroutine(convo.ShowText());
                 }
 
+            }
+            else if (Physics.Raycast(ray, out hit, range, trashMask))
+            {
+                trashObject = hit.transform.parent.gameObject;
+                trashObject.SetActive(false);
             }
         }
     }
